c68e0eb1a1528bcd03e980d01b9766f5e7d2984f
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2023-07-28 18:34:14 +0800

    Update GPlayer.py

--- GPlayer.py
+++ GPlayer.py
@@ -264,7 +264,7 @@ class GPlayer:
 			elif header == FORMAT[0]:
 				indata = indata[1:].decode()
 				print("format")
-				msg = self.BOAT_ID.to_bytes()+'\n'+'\n'.join(self.camera_format)
+				msg = self.BOAT_ID.to_bytes(1)+'\n'+'\n'.join(self.camera_format)
 				msg = FORMAT + msg.encode()
 
 				self.client.sendto(msg,(self.P_CLIENT_IP,self.OUT_PORT))
