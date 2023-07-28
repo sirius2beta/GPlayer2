5b388e0aff85b9687c4a32097e8f7a2d3823d1e5
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2023-07-28 23:49:46 +0800

    Update GPlayer.py

--- GPlayer.py
+++ GPlayer.py
@@ -163,7 +163,7 @@ class GPlayer:
 				print(f"Secondary unreached: {self.S_CLIENT_IP}:{self.OUT_PORT}")
 	def sensorLoop(self):
 		value = 0
-		num_sensor = chr(2)
+		num_sensor = chr(1)
 		
 		run = True
 		while run:
