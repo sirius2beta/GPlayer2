a8a2d196e75e84456b2182ed36bccf8002f2d658
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2023-08-01 20:51:11 +0800

    Update SensorManager.py

--- SensorManager.py
+++ SensorManager.py
@@ -3,13 +3,20 @@ class SensorManager:
 	def __init__(self):
 		self.name = 'sensor'
 		self._on_message = None
+		self._sensorList = [[1,'i']]
 	def call(self, Msg):
 		if self._on_message != None:
-			
 			on_message = self.on_message
-			on_message(Msg)
+			
+			for sensor in self._sensorList:
+				Msg = str(sensor[0])+" : "+sensor[1]
+				on_message(Msg)
+				
 		else:
 			print("not called")
+	@sensorList.setter
+	def sensorList(self, slist)
+		self._sensorList = slist
 	@property
 	def on_message(self):
 		return self._on_message
