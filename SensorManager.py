ca773a54626b979613c622e32fafe72357b51ce7
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2023-08-01 21:19:46 +0800

    Update SensorManager.py

--- SensorManager.py
+++ SensorManager.py
@@ -1,9 +1,36 @@
-
+SENSOR = b'\x50'
 class SensorManager:
 	def __init__(self):
 		self.name = 'sensor'
 		self._on_message = None
 		self._sensorList = [[1,'i']]
+
+		self.thread_sensor = threading.Thread(target=self.sensorLoop)
+		self.thread_sensor.start()
+	def sensorLoop(self):
+		value = 0
+		num_sensor = chr(1)
+
+		value += 1;
+		sensorMsg = SENSOR
+		sensorMsg += bytes(num_sensor, 'ascii')
+		sensorMsg += bytes('i', 'ascii')
+		sensorMsg+=bytes(chr(1),'ascii')
+		sensorMsg+=bytes(chr(1),'ascii')
+		sensorMsg+=value.to_bytes(4, 'big')
+		sensorMsg += bytes('i', 'ascii')
+		sensorMsg+=bytes(chr(1),'ascii')
+		sensorMsg+=bytes(chr(0),'ascii')
+		sensorMsg+=int(value/2).to_bytes(4, 'big')
+		
+		if self.thread_terminate is True:
+			break
+		try:
+			on_message = self.on_message
+			on_message(sensorMsg)
+			time.sleep(1)
+		except:
+			print(f"Sensor failed")	
 	def call(self, Msg):
 		if self._on_message != None:
 			on_message = self.on_message
@@ -16,7 +43,8 @@ class SensorManager:
 			print("not called")
 	def setSensor(self, slist):
 		self._sensorList = slist
-		print(f'setsensor: sensor:{slist[0][0]}, {slist[0][1]}')
+		for sensor in self._sensorList:
+			print(f'setsensor: sensor:{sensor[0]}, {sensor[1]}')
 	@property
 	def sensorList(self):
 		return self.sensorList
