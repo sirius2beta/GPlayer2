95cbb358f693523677a0f9d24c6a99a8b7a72a20
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2023-08-01 21:22:59 +0800

    Update SensorManager.py

--- SensorManager.py
+++ SensorManager.py
@@ -11,29 +11,30 @@ class SensorManager:
 		self.thread_terminate = True
 		self.thread_sensor.join()
 	def sensorLoop(self):
-		value = 0
-		num_sensor = chr(1)
-
-		value += 1;
-		sensorMsg = SENSOR
-		sensorMsg += bytes(num_sensor, 'ascii')
-		sensorMsg += bytes('i', 'ascii')
-		sensorMsg+=bytes(chr(1),'ascii')
-		sensorMsg+=bytes(chr(1),'ascii')
-		sensorMsg+=value.to_bytes(4, 'big')
-		sensorMsg += bytes('i', 'ascii')
-		sensorMsg+=bytes(chr(1),'ascii')
-		sensorMsg+=bytes(chr(0),'ascii')
-		sensorMsg+=int(value/2).to_bytes(4, 'big')
-		
-		if self.thread_terminate is True:
-			break
-		try:
-			on_message = self.on_message
-			on_message(sensorMsg)
-			time.sleep(1)
-		except:
-			print(f"Sensor failed")	
+		while thread_terminate == False:
+			value = 0
+			num_sensor = chr(1)
+	
+			value += 1;
+			sensorMsg = SENSOR
+			sensorMsg += bytes(num_sensor, 'ascii')
+			sensorMsg += bytes('i', 'ascii')
+			sensorMsg+=bytes(chr(1),'ascii')
+			sensorMsg+=bytes(chr(1),'ascii')
+			sensorMsg+=value.to_bytes(4, 'big')
+			sensorMsg += bytes('i', 'ascii')
+			sensorMsg+=bytes(chr(1),'ascii')
+			sensorMsg+=bytes(chr(0),'ascii')
+			sensorMsg+=int(value/2).to_bytes(4, 'big')
+			
+			if self.thread_terminate is True:
+				break
+			try:
+				on_message = self.on_message
+				on_message(sensorMsg)
+				time.sleep(1)
+			except:
+				print(f"Sensor failed")	
 	def call(self, Msg):
 		if self._on_message != None:
 			on_message = self.on_message
