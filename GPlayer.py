d1c9657928839da58de3e861d905405034124ade
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2023-08-12 12:02:49 +0800

    Update GPlayer.py

--- GPlayer.py
+++ GPlayer.py
@@ -126,7 +126,7 @@ class GPlayer:
 	def sendMsg(self, topic, msg):
 		# Send primary heartbeat every 0.5s
 		
-		msg = topic + bytes(chr(self.BOAT_ID),'ascii) + msg
+		msg = topic + bytes(chr(self.BOAT_ID),'ascii') + msg
 		print(f"topic:{msg[0]}\nmsg: {msg}")
 		try:
 			self.client.sendto(msg,(self.P_CLIENT_IP,self.OUT_PORT))
