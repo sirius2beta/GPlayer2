bd464a41aa320eaf6d9f326a34bafed0c4691d83
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2023-08-12 09:57:19 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -4,5 +4,7 @@ import SensorManager
 gplayer = GPlayer.GPlayer()
 sm = SensorManager.SensorManager()
 gplayer.on_setsensor = sm.setSensor
+gplayer.get_dev_info = sm.on_dev_info
 sm.on_message = gplayer.sendMsg
-sm.call("hello")
+
+
