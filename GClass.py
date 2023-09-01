b96e6bc87d58cdc198912b39dc200c4292ace743
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2023-09-01 21:55:14 +0800

    Update GClass.py

--- GClass.py
+++ GClass.py
@@ -1,5 +1,12 @@
 class Peripheral:
-	ID = ""
+	def __init__(self, idProduct = "", idVendor="", manufacturer="", index="", ID = 0):
+		self.idProduct = idProduct
+		self.idVendor = idVendor
+		self.manufacturer = manufacturer
+		self.index = index
+		self.ID = ID
+	
+	
 	
 
 class Device:
